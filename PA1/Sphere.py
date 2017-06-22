4b07e31a2d3f0682fc73c11058071e48ac5ec389
Carlos Barillas <cbarillas561@gmail.com>
2017-06-21 18:37:30 -0700

    changed print method

--- PA1/Sphere.py
+++ PA1/Sphere.py
@@ -1,7 +1,7 @@
 # Carlos Barillas
 # cbarilla@ucsc.edu
 # Programming Assignment 1
-# Computes the volume and surface area of a sphere whose radius is entered by user
+# Computes the volume and surface area of a sphere whose radius is entered by user.
 
 from math import pi as p
 
@@ -9,6 +9,6 @@ radius = float(input("Enter the radius of the sphere: "))
 surfaceArea = 4* p*radius**2
 volume = 4/3*p*radius**3
 
-print("The volume is: "+str(volume))
-print("The surface area is: "+str(surfaceArea))
+print("The volume is: {}".format(volume))
+print("The surface area is: {}".format(surfaceArea))
 
