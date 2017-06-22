cde066e99042af2923abffac70ed9ea946d5c0a5
Carlos Barillas <cbarillas561@gmail.com>
2017-06-21 21:07:19 -0700

    cleaned up comments and names

--- PA4/Primes.py
+++ PA4/Primes.py
@@ -1,42 +1,42 @@
 # Carlos Barillas
 # cbarilla@ucsc.edu
 # Program Assignment 4
-# This program checks a positive integer n from user input then prints out the first n prime numbers
+# This program checks a positive integer n from user input then prints out the first n prime numbers.
 
 import math
 
 def isPrime(m, L):
     for number in L:
-        if m % number == 0:           # Checks if m is composite.
+        if m % number == 0:            # Checks if m is composite.
             return False
-    return True                       # If m is not divisible by any number in L, m is prime.
+    return True                        # If m is not divisible by any number in L, m is prime.
 
 numberOfPrimes = int(input("Enter the number of Primes to compute: "))
 print()
-primeList = [2]                       #primeList holds at least one prime number
+primeList = [2]                        # primeList holds at least one prime number.
 sqrtN = int(math.sqrt(numberOfPrimes))
 
-for number in range(3, sqrtN+1):      #Initializes primeList to store several prime 2 <= p <= sqrt(n)
-    prime = True                      #True if number is prime.
+for number in range(3, sqrtN+1):       # Initializes primeList to store several prime 2 <= p <= sqrt(n).
+    primeFound = True                  # True if number is prime.
     for i in range(2, number):
-        if (number % i == 0):         #Divides number by i from 2 to number to see if number is composite
-            prime = False
-    if prime:
-        primeList.append(number)      #append number to primeList if True(i.e. number is prime)
+        if (number % i == 0):          # Divides number by i from 2 to number to see if number is composite.
+            primeFound = False
+    if primeFound:
+        primeList.append(number)       # Append number to primeList if True(i.e. number is prime).
 
 print("The first {} primes are: ".format(numberOfPrimes))
-pListLength = len(primeList)          #length of primeList
+pListLength = len(primeList)           # Length of primeList.
 testPrime = 3
 while pListLength < numberOfPrimes:
-    if isPrime(testPrime, primeList): #if testprime number is prime append to primeList
+    if isPrime(testPrime, primeList):  # If testprime number is prime append to primeList.
         primeList.append(testPrime)
-        pListLength += 1              #increase length of list
-    testPrime += 2                    #increases test prime by 2
+        pListLength += 1               # Increase length of list.
+    testPrime += 2                     # Increases test prime by 2.
 
-for i, prime in enumerate(primeList):  #enumerates our list so we can use an index and items in primeList
-    print(prime, '', end ='')          #prints out each prime separated by space, end appends a space instead of new line
+for i, prime in enumerate(primeList):  # Enumerates our list so we can use an index and items in primeList.
+    print(prime, '', end ='')          # Prints out each prime separated by space, end appends a space instead of new line.
 
-    if (i+1) % 10 == 0:                #if index of primeList is divisible by 10 then print new line
+    if (i+1) % 10 == 0:                # If index of primeList is divisible by 10 then print new line.
         print()
 
-print('\n')
+print()
