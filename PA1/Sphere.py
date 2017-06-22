f7114473e8d54839e2184e2d9bf73ba1f26d5e1f
Carlos Barillas <cbarillas561@gmail.com>
2017-06-21 20:37:58 -0700

    fixed spacing problem

--- PA1/Sphere.py
+++ PA1/Sphere.py
@@ -1,14 +1,14 @@
 # Carlos Barillas
 # cbarilla@ucsc.edu
 # Programming Assignment 1
-# Computes the volume and surface area of a sphere whose radius is entered by user.
+# Computes the volume and surface area of a sphere whose radius is entered by user
 
 from math import pi as p
 
 radius = float(input("Enter the radius of the sphere: "))
-surfaceArea = 4*p*radius**2
+surfaceArea = 4* p*radius**2
 volume = 4/3*p*radius**3
 
-print("The volume is: {}".format(volume))
-print("The surface area is: {}".format(surfaceArea))
+print("The volume is: "+str(volume))
+print("The surface area is: "+str(surfaceArea))
 
